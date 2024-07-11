c35bcf8b65576e5a27607f7900a88ba6021fb6c6
kevinlam0 <kevinclam02@gmail.com>
2024-07-10 20:01:50 -0400

    Initialized Java Backend

---
+++ pitch-backend/src/main/java/com/example/pitch_backend/PitchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pitch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchBackendApplication.class, args);
+	}
+
+}
---
+++ pitch-backend/src/test/java/com/example/pitch_backend/PitchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pitch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
